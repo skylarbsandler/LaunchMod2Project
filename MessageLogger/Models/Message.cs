2c99faacfac71bf62b18690138b6a07ee682c3bc
skylarbsandler <skylarbsandler@gmail.com>
2023-06-14 11:31:19 -0600

    Final

--- MessageLogger/Models/Message.cs
+++ MessageLogger/Models/Message.cs
@@ -11,11 +11,21 @@ namespace MessageLogger.Models
         public int Id { get; set; }
         public string Content { get; private set; }
         public DateTime CreatedAt { get; private set; }
+        public User User { get; set; }
 
         public Message(string content)
         {
             Content = content;
-            CreatedAt = DateTime.Now;
+            CreatedAt = DateTime.Now.ToUniversalTime();
         }
     }
+        
+
+    ////local time method
+    //public void ConvertTZ()
+    //    {
+    //        message.CreatedAt = TimeZoneInfo.ConvertTimeFromUtc(message.CreatedAt, timeZone);
+    //    }
+    ////return datetime, convert to local time
+    //}
 }
--- MessageLogger/Program.cs
+++ MessageLogger/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MessageLogger.Data;
 using MessageLogger.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 Console.WriteLine("Welcome to Message Logger!");
 Console.WriteLine();
@@ -9,7 +12,11 @@ string name = Console.ReadLine();
 Console.Write("What is your username? (one word, no spaces!) ");
 string username = Console.ReadLine();
 User user = new User(name, username);
-//Save/create user to DB
+using (var context = new MessageLoggerContext())
+{
+    context.Users.Add(user);
+    context.SaveChanges();
+}
 
 Console.WriteLine();
 Console.WriteLine("To log out of your user profile, enter `log out`.");
@@ -19,16 +26,26 @@ Console.Write("Add a message (or `quit` to exit): ");
 
 string userInput = Console.ReadLine();
 //Save message to Db
+using (var context = new MessageLoggerContext())
+{
+    User databaseUser = context.Users.Find(user.Id);
+    Message userMessage = new Message(userInput);
+    userMessage.User = databaseUser;
+    context.Messages.Add(userMessage);
+    context.SaveChanges();
+}
 List<User> users = new List<User>() { user };
 
 while (userInput.ToLower() != "quit")
 {
-    while (userInput.ToLower() != "log out")
+    while (userInput.ToLower() != "log out") //does not respond to 'quit'
     {
-        user.Messages.Add(new Message(userInput));
+        user.Messages.Add(new Message(userInput)); //if 'log out', do not add
 
         foreach (var message in user.Messages)
         {
+            //string timeZoneId = "Mountain Standard Time";
+            //ConvertTZ(message, timeZoneId);
             Console.WriteLine($"{user.Name} {message.CreatedAt:t}: {message.Content}");
         }
 
@@ -36,7 +53,26 @@ while (userInput.ToLower() != "quit")
 
         userInput = Console.ReadLine();
         //Save message to Db
+        Message newMessage = new Message(userInput);
+        using (var context = new MessageLoggerContext())
+        {
+            User databaseUser = context.Users.Find(user.Id);
+            Message userMessage = new Message(userInput);
+            userMessage.User = databaseUser;
+            context.Messages.Add(userMessage);
+            context.SaveChanges();
+        }
         //Read previous messages from Db
+        using (var context = new MessageLoggerContext())
+        {
+            var previousMessages = context.Users
+                 .Include(user => user.Messages)
+                 .Where(user => user.Username == username).First();
+            foreach (var message in previousMessages.Messages) //one user's messages
+            {
+                Console.WriteLine($"{user.Name} {message.CreatedAt:t}: {message.Content}");
+            }
+        }
         Console.WriteLine();
     }
 
@@ -50,12 +86,36 @@ while (userInput.ToLower() != "quit")
         username = Console.ReadLine();
         user = new User(name, username);
         //Save user to Db
+        using (var context = new MessageLoggerContext())
+        {
+            context.Users.Add(user);
+            context.SaveChanges();
+        }
         users.Add(user);
         Console.Write("Add a message: ");
 
         userInput = Console.ReadLine();
         //Save message to Db
+        Message newMessage = new Message(userInput);
+        using (var context = new MessageLoggerContext())
+        {
+            User databaseUser = context.Users.Find(user.Id);
+            Message userMessage = new Message(userInput);
+            userMessage.User = databaseUser;
+            context.Messages.Add(userMessage);
+            context.SaveChanges();
+        }
         //Read previous messages from Db
+        using (var context = new MessageLoggerContext())
+        {
+            var previousMessages = context.Users
+                 .Include(user => user.Messages)
+                 .Where(user => user.Username == username).First();
+            foreach (var message in previousMessages.Messages) //one user's messages
+            {
+                Console.WriteLine($"{user.Name} {message.CreatedAt:t}: {message.Content}");
+            }
+        }
 
     }
     else if (userInput.ToLower() == "existing")
@@ -63,20 +123,43 @@ while (userInput.ToLower() != "quit")
         Console.Write("What is your username? ");
         username = Console.ReadLine();
         user = null;
-        foreach (var existingUser in users)
+        //Read Db for existing user
+        using (var context = new MessageLoggerContext())
         {
-            if (existingUser.Username == username)
+            foreach (var existingUser in context.Users)
             {
-                user = existingUser;
+                if (existingUser.Username == username)
+                {
+                    user = existingUser;
+                }
             }
         }
-        
+
         if (user != null)
         {
             Console.Write("Add a message: ");
             userInput = Console.ReadLine();
             //Save message to Db
+            Message newMessage = new Message(userInput);
+            using (var context = new MessageLoggerContext())
+            {
+                User databaseUser = context.Users.Find(user.Id);
+                Message userMessage = new Message(userInput);
+                userMessage.User = databaseUser;
+                context.Messages.Add(userMessage);
+                context.SaveChanges();
+            }
             //Read previous messages from Db
+            using (var context = new MessageLoggerContext())
+            {
+                var previousMessages = context.Users
+                     .Include(user => user.Messages)
+                     .Where(user => user.Username == username).First();
+                foreach (var message in previousMessages.Messages) //one user's messages
+                {
+                    Console.WriteLine($"{user.Name} {message.CreatedAt:t}: {message.Content}");
+                }
+            }
         }
         else
         {
